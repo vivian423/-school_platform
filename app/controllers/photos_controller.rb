7648be6dd96e45092da44d7d72cb23c477fe834a
adrianmihai <mihaiadriancosmin@yahoo.com>
2023-03-21 14:14:46 +0100

    setup done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,21 @@
 class PhotosController < ApplicationController
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.save
+    redirect_to photos_path
+  end
+  def index
+    @photos = Photo.all
+  end
+
+    private
+
+    def photo_params
+      params.require(:photo).permit(:photo)
+    end
 end
