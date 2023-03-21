2edc16fbed25bfbd02bc98eb54b4bcc113aebffe
Vivian Chan <vivianchan423@gmail.com>
2023-03-21 17:27:32 +0100

    dashboard moved to pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,13 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    @kids = current_user.children
+    @activities = Activity.find_by(date: Date.today)
+    @chatrooms = Chatroom.all
+    @photos = Photo.all
+    @events = Event.all
+    # @announcements = Announcement.all
+  end
 end
