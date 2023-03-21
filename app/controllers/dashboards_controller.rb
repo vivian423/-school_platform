b7182e832190e2ea3588a7106339c056380d6d2c
alec strobel <astrobel96@googlemail.com>
2023-03-21 14:18:16 +0100

    Completed dashboard controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,8 +1,10 @@
 class DashboardsController < ApplicationController
-
   def dashboard
-    @activities = Activity.find_by(Date.today)
-    @kid = current_user.parent.kid
+    @kids = current_user.children
+    @activities = Activity.find_by(date: Date.today)
+    @chatrooms = Chatroom.all
+    @photos = Photo.all
+    @events = Event.all
+    @announcements = Announcement.all
   end
-
 end
