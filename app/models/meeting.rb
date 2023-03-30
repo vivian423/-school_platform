c3b0683635f6018d59232e2771d2c14eb2b346b2
alec strobel <astrobel96@googlemail.com>
2023-03-30 09:13:54 +0200

    Updating for merge

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -5,4 +5,13 @@ class Meeting < ApplicationRecord
   validates :end_time, comparison: { greater_than: :start_time }
 
   has_noticed_notifications
+  after_create_commit { broadcast_notifications }
+
+  private
+
+  def broadcast_notifications
+    User.all.each do |user|
+      UserNotification.with(type: "meeting").deliver(user) if user.teacher == false
+    end
+  end
 end
